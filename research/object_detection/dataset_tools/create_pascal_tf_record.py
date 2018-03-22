3db7bb04e6cb3c86bfd4ee254359278a21ed2ade
Aswin Chandrasekaran <aswincsekar@users.noreply.github.com>
2018-03-22 19:57:36 +0530

    new

--- research/object_detection/dataset_tools/create_pascal_tf_record.py
+++ research/object_detection/dataset_tools/create_pascal_tf_record.py
@@ -161,22 +161,26 @@ def main(_):
 
   for year in years:
     logging.info('Reading from PASCAL %s dataset.', year)
-    examples_path = os.path.join(data_dir, year, 'ImageSets', 'Main',
-                                 'aeroplane_' + FLAGS.set + '.txt')
-    annotations_dir = os.path.join(data_dir, year, FLAGS.annotations_dir)
-    examples_list = dataset_util.read_examples_list(examples_path)
-    # for idx, example in enumerate(examples_list):
-    #   if idx % 100 == 0:
-    #     logging.info('On image %d of %d', idx, len(examples_list))
-    #   path = os.path.join(annotations_dir, example + '.xml')
-    #   with tf.gfile.GFile(path, 'r') as fid:
-    #     xml_str = fid.read()
-    #   xml = etree.fromstring(xml_str)
-    #   data = dataset_util.recursive_parse_xml_to_dict(xml)['annotation']
-    #
-    #   tf_example = dict_to_tf_example(data, FLAGS.data_dir, label_map_dict,
-    #                                   FLAGS.ignore_difficult_instances)
-    #   writer.write(tf_example.SerializeToString())
+    for label in label_map_dict.keys():
+        image_set_path = os.path.join(data_dir, year, 'ImageSets', 'Main',
+                                      label+ '_' + FLAGS.set + '.txt')
+        # examples_path = os.path.join(data_dir, year, 'ImageSets', 'Main',
+        #                              'aeroplane_' + FLAGS.set + '.txt')
+        annotations_dir = os.path.join(data_dir, year, FLAGS.annotations_dir)
+        examples_list = dataset_util.read_examples_list(image_set_path)
+        print(examples_list)
+        # for idx, example in enumerate(examples_list):
+        #   if idx % 100 == 0:
+        #     logging.info('On image %d of %d', idx, len(examples_list))
+        #   path = os.path.join(annotations_dir, example + '.xml')
+        #   with tf.gfile.GFile(path, 'r') as fid:
+        #     xml_str = fid.read()
+        #   xml = etree.fromstring(xml_str)
+        #   data = dataset_util.recursive_parse_xml_to_dict(xml)['annotation']
+        #
+        #   tf_example = dict_to_tf_example(data, FLAGS.data_dir, label_map_dict,
+        #                                   FLAGS.ignore_difficult_instances)
+        #   writer.write(tf_example.SerializeToString())
 
   writer.close()
 
